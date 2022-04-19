6442917264f48789ee318533abb55d9145591c4a
aiaaua <yse00800@gmail.com>
2022-04-19 16:11:22 +0900

    update system programming chapter6-1, upload system programming example

---
+++ system_programming/example/sharedmemory.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+	int shmid, pid;
+	char *shmaddr_parent, *shmaddr_child;
+	shmid = shmget((key_t)1234, 10, IPC_CREAT|0644);
+	if(shmid == -1) {
+		perror("shmget error\n");
+		exit(1);
+	}
+	pid = fork();
+	if(pid > 0) {
+		wait(0);
+		shmaddr_parent = (char *)shmat(shmid, (char *)NULL, 0);
+		printf("%s\n", shmaddr_parent);
+		shmdt((char *)shmaddr_parent);
+	}
+	else {
+		shmaddr_child = (char *)shmat(shmid, (char *)NULL, 0);
+		strcpy((char *)shmaddr_child, "Hello Parent!");
+		shmdt((char *)shmaddr_child);
+		exit(0);
+	}
+	shmctl(shmid, IPC_RMID, (struct shmid_ds *)NULL);
+	return 0;
+}	
